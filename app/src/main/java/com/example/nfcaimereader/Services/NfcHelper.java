fd9b8f44ad9f1635be23256a23b21d862559ba06
Takagi-XT <1766929540@qq.com>
2024-04-24 01:12:47 +0800

    Add Toast import and update NFC helper setup in NfcHelper.java

--- app/src/main/java/com/example/nfcaimereader/Services/NfcHelper.java
+++ app/src/main/java/com/example/nfcaimereader/Services/NfcHelper.java
@@ -5,13 +5,13 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
+import android.widget.Toast;
 
 import com.example.nfcaimereader.MainActivity;
 
 public class NfcHelper {
     // NFC
     private final NfcAdapter nfcAdapter;
-
     private final PendingIntent pendingIntent;
     private final IntentFilter[] intentFiltersArray;
     private final String[][] techListsArray;
@@ -19,13 +19,17 @@ public class NfcHelper {
     public NfcHelper(Activity activity) {
         // 检查设备是否支持NFC
         nfcAdapter = NfcAdapter.getDefaultAdapter(activity);
+        if (nfcAdapter == null) {
+            // 设备不支持NFC
+            Toast.makeText(activity, "This device does not support NFC.", Toast.LENGTH_SHORT).show();
+        }
 
         pendingIntent = PendingIntent.getActivity(
                 activity, 0, new Intent(activity, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
                 PendingIntent.FLAG_MUTABLE
         );
-        IntentFilter tech = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
 
+        IntentFilter tech = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
         intentFiltersArray = new IntentFilter[]{tech};
 
         techListsArray = new String[][]{
@@ -35,14 +39,18 @@ public class NfcHelper {
     }
 
     public void enableNfcScan(Activity activity) {
-        nfcAdapter.enableForegroundDispatch(activity, pendingIntent, intentFiltersArray, techListsArray);
+        if (nfcAdapter != null) {
+            nfcAdapter.enableForegroundDispatch(activity, pendingIntent, intentFiltersArray, techListsArray);
+        }
     }
 
     public void disableNfcScan(Activity activity) {
-        nfcAdapter.disableForegroundDispatch(activity);
+        if (nfcAdapter != null) {
+            nfcAdapter.disableForegroundDispatch(activity);
+        }
     }
 
     public boolean isNfcEnabled() {
-        return nfcAdapter.isEnabled();
+        return nfcAdapter != null && nfcAdapter.isEnabled();
     }
 }
