62ac805f72b2d39c1bf84290dd8fcbd81a851a4a
ohdj <71207981+ohdj@users.noreply.github.com>
2024-09-10 09:45:42 +0800

    chore: 把旧项目删了重做
    BREAKING CHANGE: 传统View架构->Compose架构

---
+++ NfcAimeReader.Android/app/src/main/java/org/ohdj/nfcaimereader/MainActivity.kt
@@ -0,0 +1,44 @@
+package org.ohdj.nfcaimereader
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import org.ohdj.nfcaimereader.utils.NetworkScanner
+import org.ohdj.nfcaimereader.utils.NfcManager
+import org.ohdj.nfcaimereader.views.MainScreen
+
+class MainActivity : ComponentActivity() {
+    private lateinit var nfcManager: NfcManager
+    private lateinit var networkScanner: NetworkScanner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        nfcManager = NfcManager(this)
+        networkScanner = NetworkScanner()
+
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen(nfcManager, networkScanner)
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        nfcManager.enableNfcReaderMode()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        nfcManager.disableNfcReaderMode()
+    }
+}
