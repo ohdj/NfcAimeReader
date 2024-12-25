9be17955e8bcb7c40eaacb2bffceb75a5340497a
ohdj <71207981+ohdj@users.noreply.github.com>
2024-12-25 22:34:35 +0800

    style(UI): enableEdgeToEdge

--- NfcAimeReader.Android/app/src/main/java/org/ohdj/nfcaimereader/MainActivity.kt
+++ NfcAimeReader.Android/app/src/main/java/org/ohdj/nfcaimereader/MainActivity.kt
@@ -2,7 +2,9 @@ package org.ohdj.nfcaimereader
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,6 +17,16 @@ class MainActivity : ComponentActivity() {
     private lateinit var networkScanner: NetworkScanner
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                android.graphics.Color.TRANSPARENT,
+                android.graphics.Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                android.graphics.Color.TRANSPARENT,
+                android.graphics.Color.TRANSPARENT
+            )
+        )
         super.onCreate(savedInstanceState)
         nfcManager = NfcManager(this)
         networkScanner = NetworkScanner()
