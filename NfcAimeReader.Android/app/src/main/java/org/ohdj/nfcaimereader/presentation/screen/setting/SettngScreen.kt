8f7f7596345fe3ed920ee441ac6ea70ccd45f79f
ohdj <71207981+ohdj@users.noreply.github.com>
2025-02-25 00:05:17 +0800

    feat(UI): 好看点的设置页

--- NfcAimeReader.Android/app/src/main/java/org/ohdj/nfcaimereader/presentation/screen/setting/SettngScreen.kt
+++ NfcAimeReader.Android/app/src/main/java/org/ohdj/nfcaimereader/presentation/screen/setting/SettngScreen.kt
@@ -1,42 +1,69 @@
 package org.ohdj.nfcaimereader.presentation.screen.setting
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingScreen(viewModel: SettingViewModel) {
     val context = LocalContext.current
+
     LaunchedEffect(Unit) {
         viewModel.loadAutoReconnect(context)
         viewModel.loadUseSystemTheme(context)
     }
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Text(text = "启用自动重连")
-            Spacer(modifier = Modifier.width(8.dp))
-            Switch(
-                checked = viewModel.autoReconnect.collectAsState().value,
-                onCheckedChange = { viewModel.setAutoReconnect(context, it) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("设置")
+                }
             )
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Text(text = "使用系统主题颜色")
-            Spacer(modifier = Modifier.width(8.dp))
-            Switch(
-                checked = viewModel.useSystemTheme.collectAsState().value,
-                onCheckedChange = { viewModel.setUseSystemTheme(context, it) }
+        },
+    ) { contentPadding ->
+        Column(
+            modifier = Modifier.padding(contentPadding)
+        ) {
+            ListItem(
+                headlineContent = {
+                    Text(
+                        text = "主题",
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+            )
+            ListItem(
+                headlineContent = { Text("自动重连") },
+                supportingContent = { Text("游戏连接断开后尝试重连") },
+                trailingContent = {
+                    Switch(
+                        checked = viewModel.autoReconnect.collectAsState().value,
+                        onCheckedChange = { viewModel.setAutoReconnect(context, it) }
+                    )
+                }
+            )
+            ListItem(
+                headlineContent = { Text("动态取色") },
+                supportingContent = { Text("使用系统提供的配色方案") },
+                trailingContent = {
+                    Switch(
+                        checked = viewModel.useSystemTheme.collectAsState().value,
+                        onCheckedChange = { viewModel.setUseSystemTheme(context, it) }
+                    )
+                }
             )
         }
     }
